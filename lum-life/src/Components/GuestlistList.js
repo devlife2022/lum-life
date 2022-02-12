5678b33fa4dda35ddc6a6b11ed80a6c07d7dc75d
devlife2022 <devlife2022@gmail.com>
2022-02-12 17:04:10 -0500

    Firestore Data Working
    Firestore data is now loading to the list.  Need to work on styling next.

--- lum-life/src/Components/GuestlistList.js
+++ lum-life/src/Components/GuestlistList.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -7,11 +7,26 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
 import Grid from '@mui/material/Grid';
-
+import firebase from "firebase/compat/app";
 
 
 
 export default function GuestlistList() {
+
+const [guestlists, setGuestlists] = useState();
+
+useEffect(() => {
+firebase.firestore()
+.collection("guestlists")
+.get()
+.then(guestlists => {
+	console.log(guestlists.docs.event_name);
+	setGuestlists(guestlists.docs)
+	console.log(guestlists.docs[0].data())
+});
+}, []);
+
+
   return (
   <Grid
   	container
@@ -23,7 +38,25 @@ export default function GuestlistList() {
   	<Grid item>
 	    <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper', margin: 4}}>
 	      <nav aria-label="secondary mailbox folders">
-	        <List>
+	        <List disablePadding>
+
+	        {
+    		guestlists && guestlists.map(guestlist => {
+    		return(
+    			<div>
+    			<ListItem disablePadding key={guestlist.id}>
+    				 <ListItemButton>
+    				 	<ListItemText>
+    				 	{new Date(guestlist.data().event_date.seconds * 1000).toLocaleDateString("en-us")} - {guestlist.data().event_name} - {guestlist.data().event_city}
+    				 	</ListItemText>
+    			 	</ListItemButton>
+
+	          </ListItem>
+	          <Divider />
+	          </div>
+	          )
+    	})}
+    		{/*
 	          <ListItem disablePadding>
 	            <ListItemButton>
 	              <ListItemText primary="Testing Testing Testing" />
@@ -35,9 +68,13 @@ export default function GuestlistList() {
 	              <ListItemText primary="Spam" />
 	            </ListItemButton>
 	          </ListItem>
+	         */}
 	        </List>
 	      </nav>
 	    </Box>
+    </Grid>
+    <Grid item>
+    	
     </Grid>
    </Grid>
   );
