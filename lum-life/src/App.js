43882d1c2ea57edeb3272d667abf6798b6eb8670
devlife2022 <devlife2022@gmail.com>
2022-02-12 12:42:08 -0500

    App Bar Header
    First pass at the app bar header.  Links not functioning yet, just wanted to get the foundation setup for now.

--- lum-life/src/App.js
+++ lum-life/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import ResponsiveAppBar from './Components/Header.js';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
+  return ( 
+              
+    <div>
+      <ResponsiveAppBar />
     </div>
   );
 }
